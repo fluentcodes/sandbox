c83693ad778c0ac3a6843eaae240ce68c8c9d2ee
Diwischek <fluentcodes@gmail.com>
2020-09-03 11:46:05 +0200

    java: Added LinkedHashMapTest from java 8 repository

--- src/test/java/fluentcodes/sandbox/util/LinkedHashMapTest.java
+++ src/test/java/fluentcodes/sandbox/util/LinkedHashMapTest.java
@@ -3,6 +3,7 @@ package fluentcodes.sandboxjava.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -17,4 +18,35 @@ public class LinkedHashMapTest {
     map.remove("test");
     Assert.assertEquals(0, map.size());
   }
+
+    @Test
+    public void test() {
+      Map<String,Integer> linkedHashMap = new LinkedHashMap<>();
+      linkedHashMap.put("a",1);
+      linkedHashMap.put("c",2);
+      linkedHashMap.put("b",3);
+      linkedHashMap.put("x",4);
+      linkedHashMap.put("z",5);
+      linkedHashMap.put("y",6);
+      for (int i=0;i<1000;i++) {
+        linkedHashMap.put(new Integer(i).toString(),i);
+      }
+      for (String key:linkedHashMap.keySet()) {
+        System.out.println(key + "=" + linkedHashMap.get(key));
+      }
+
+      Map<String,Integer> map = new HashMap<>();
+      map.put("a",1);
+      map.put("c",2);
+      map.put("b",3);
+      map.put("x",4);
+      map.put("z",5);
+      map.put("y",6);
+      for (int i=0;i<1000;i++) {
+        map.put(new Integer(i).toString(),i);
+      }
+      for (String key:map.keySet()) {
+        System.out.println(key + "=" + map.get(key));
+      }
+    }
 }
