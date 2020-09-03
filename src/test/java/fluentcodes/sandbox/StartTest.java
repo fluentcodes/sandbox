76e3b568b8a70c8b11658eed5429a44cbdfac4ad
Diwischek <fluentcodes@gmail.com>
2020-09-03 10:36:10 +0200

    Fix in pom and directory template directory structure

---
+++ src/test/java/fluentcodes/sandbox/StartTest.java
@@ -0,0 +1,14 @@
+package fluentcodes.sandbox;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Just to check if dependencies are ok
+ */
+public class StartTest {
+    @Test
+    public void check () {
+        Assert.assertEquals(1, 1);
+    }
+}
